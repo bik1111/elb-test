2749d1e4c704cbd39dcc8b3b1710a145ceccbab5
Jin Cheol <bik1111@khu.ac.kr>
2023-07-12 23:26:26 +0900

    test elb

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.send("<h1>메롱 약오르지 까꿍! </h1>")
+})
+
+app.get('/health', (req,res) => {
+    res.send("I m really HEALTY BOY!~~")
+})
+
+app.listen(port, () => {
+    console.log("SERVER LISTEING ON 3001!")
+}) 
+
